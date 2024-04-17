719918d32de362f89d814e24e6a3f7f9c880ad0c
Daivaras <daivariux321@gmail.com>
2024-04-17 15:50:22 +0300

    Project list css added

--- front/src/components/Project.jsx
+++ front/src/components/Project.jsx
@@ -1,16 +1,33 @@
-
+import "../styles/Project.css";
+import { useState, useEffect } from "react";
 
 function Project({ project }) {
-    
-    const { _id, projectName, icon, description, status, overall} = project;
-    return(
-        <div key={_id}>
-            <p>{icon}</p>
-            <p>{projectName}</p>
-            <p>{status}</p>
-            <p>{overall}</p>
-        </div>
-    )
+  const [statusCheck, setStatusCheck] = useState("");
+
+  const { _id, projectName, icon, description, status, overall } = project;
+ 
+  useEffect(() => {
+    setStatusCheck(status);
+  }, [])
+
+  return (
+    <div key={_id} className="project-list-box">
+      <tr className="project-list">
+        <th className="project-icon">
+          <img src={icon} alt="" />
+        </th>
+        <th className="project-name">
+          <p>{projectName}</p>
+        </th>
+        <th className={statusCheck=="done" ? "project-done" : "" || statusCheck=="on hold" ? "project-onHold" : "" || statusCheck=="in progress" ? "project-inProgress" : ""}>
+          <p>{status}</p>
+        </th>
+        <th className="project-overall">
+          <p>{overall}</p>
+        </th>
+      </tr>
+    </div>
+  );
 }
 
 export default Project;
--- front/src/components/ProjectList.jsx
+++ front/src/components/ProjectList.jsx
@@ -1,14 +1,35 @@
 import Project from "./Project";
+import "../styles/ProjectList.css";
 
-function ProjectList({projects, error}) {
-    return(
-        <>
-        {projects.map((project) => {
-        return <Project project={project} key={project._id}/>
-        })}
-        {error}
-        </>
-    );
+function ProjectList({ projects, error }) {
+  return (
+    <div className="corner-gaps">
+    <table className="table-box">
+        <tr className="project-title">
+          <th className="invinsible"></th>
+          <th className="name">PROJECT NAME</th>
+          <th className="status">STATUS</th>
+          <th className="tasks">TASKS</th>
+        </tr>
+        <tr>
+          <th>
+            {projects.map((project) => {
+              return (
+                <Project
+                  className="project-list"
+                  project={project}
+                  key={project._id}
+                />
+                
+              );
+            })}
+            {error}{" "}
+            </th>
+        </tr>
+      <tbody></tbody>
+    </table>
+    </div>
+  );
 }
 
 export default ProjectList;
