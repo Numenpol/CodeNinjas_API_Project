cbb71e15458917b2d6e198e52a197fcb6a67dda7
Numenpol <dealyreal@gmail.com>
2024-04-19 11:02:15 +0300

    merged projectlist with projectpage

---
+++ front/src/components/ProjectLists.jsx
@@ -0,0 +1,36 @@
+import Project from "./Project";
+import "../styles/ProjectList.css";
+
+function ProjectList({ projects, error }) {
+  return (
+    <div className="corner-gaps">
+      <table className="table-box">
+        <div className="project-list-box">
+        <thead>
+          <tr className="project-title">
+            <th className="invinsible"></th>
+            <th className="name">PROJECT NAME</th>
+            <th className="status">STATUS</th>
+            <th className="tasks">TASKS</th>
+          </tr>
+        </thead>
+        <tbody>
+          {projects.map((project) => {
+            return (
+              <Project
+                className="project-list"
+                project={project}
+                key={project._id}
+              />
+            );
+          })}
+          {error}{" "}
+         
+        </tbody>
+        </div>
+      </table>
+    </div>
+  );
+}
+
+export default ProjectList;
