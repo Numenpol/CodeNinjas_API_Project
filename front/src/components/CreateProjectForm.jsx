0cf137e92ce4ffaf2af3fd7b7f939f7577e5521a
elvis <laimingaz@yahoo.it>
2024-04-24 14:31:28 +0300

    fixed menu popup css

--- front/src/components/CreateProjectForm.jsx
+++ front/src/components/CreateProjectForm.jsx
@@ -1,17 +1,17 @@
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import { useContext } from "react";
 import { StateContext } from "../utils/StateContext";
 import { useForm } from "react-hook-form";
 import Form from "react-bootstrap/Form";
 import { postData } from "../services/post";
 import IconList from "../components/IconList";
-import "../styles/createProjectForm.css"
+import "../styles/createProjectForm.css";
 import xIcon from "../assets/xIcon.svg";
 import rocket from "../assets/rocket.svg";
 
 function CreateProjectForm() {
-  const {show, setShow, icon} = useContext(StateContext)
+  const { show, setShow, icon } = useContext(StateContext);
 
   const {
     register,
@@ -25,7 +25,6 @@ function CreateProjectForm() {
     },
   });
 
-
   const formSubmitHandler = async (data) => {
     try {
       await postData({ ...data, icon: icon });
@@ -34,98 +33,106 @@ function CreateProjectForm() {
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const handleClose = () => {
-    setShow(false)
+    setShow(false);
     reset();
-    };
+  };
 
-  
   return (
     <>
       <Modal
         show={show}
         onHide={handleClose}
-        backdrop="static"
         keyboard={false}
+        backdrop="static"
+        className="create-project-modal"        
+        bsPrefix="create-project-modal"
       >
         <div className="create-project">
-        <button className="xIconButton" onClick={handleClose}>
-            <img src={xIcon} alt="xIcon" />
-        </button>    
-        <h1 className="H12">Create a new project</h1>
-        <Form onSubmit={handleSubmit(formSubmitHandler)} className="create-project--form">
           <div>
-            <Form.Group
-              className="NewProjectName"
-              controlId="exampleForm.ControlInput1"
+            <button className="xIconButton" onClick={handleClose}>
+              <img src={xIcon} alt="xIcon" />
+            </button>
+            <h1 className="H12">Create a new project</h1>
+            <Form
+              onSubmit={handleSubmit(formSubmitHandler)}
+              className="create-project--form"
             >
-              <Form.Label>Project Name</Form.Label>
-              <Form.Control
-                type="textarea"
-                placeholder="New project"
-                autoComplete="projectName"
-                {...register("projectName", {
-                  required: "Project name is required",
-                })}
-                isInvalid={errors.projectName}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.projectName && errors.projectName.message}
-              </Form.Control.Feedback>
-            </Form.Group>
+              <div>
+                <Form.Group
+                  className="NewProjectName"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label>Project Name</Form.Label>
+                  <Form.Control
+                    type="textarea"
+                    placeholder="New project"
+                    autoComplete="projectName"
+                    autoFocus
+                    {...register("projectName", {
+                      required: "Project name is required",
+                    })}
+                    isInvalid={errors.projectName}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.projectName && errors.projectName.message}
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </div>
+              <div>
+                <p className="create-choose--icon">Choose your project icon</p>
+                <IconList />
+              </div>
+              <div>
+                <Form.Group
+                  className="NewProjectDesc"
+                  controlId="exampleForm.ControlTextarea1"
+                >
+                  <Form.Label>Project description</Form.Label>
+                  <Form.Control
+                    type="textarea"
+                    as="textarea"
+                    rows={3}
+                    placeholder="Project description"
+                    autoComplete="description"
+                    {...register("description", {
+                      required: "Project description is required",
+                    })}
+                    isInvalid={errors.description}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.description && errors.description.message}
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </div>
+              <div className="CreateButtons">
+                <Button
+                  className="cancelBtn"
+                  variant="primary"
+                  onClick={handleClose}
+                >
+                  <div className="cancelBtnContent">Cancel</div>
+                </Button>
+                <Button
+                  variant="primary"
+                  className="createBtn"
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  <div className="createBtnContent">Create</div>
+                </Button>
+              </div>
+            </Form>
           </div>
-          <div>
-            <p className="create-choose--icon">Choose your project icon</p>
-            <IconList/>
+          <div className="RocketThing">
+            <div className="rocketPicture">
+              <img src={rocket} alt="rocketPicture" className="rocket" />
+              <h1 className="rocketSlogan">Ready? Launch!</h1>
+            </div>
           </div>
-          <div>
-            <Form.Group
-              className="NewProjectDesc"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Project description</Form.Label>
-              <Form.Control
-                type="textarea"
-                as="textarea"
-                rows={3}
-                placeholder="Project description"
-                autoComplete="description"
-                {...register("description",
-                  {
-                    required: "Project description is required",
-                  })}
-                isInvalid={errors.description}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.description && errors.description.message}
-              </Form.Control.Feedback>
-            </Form.Group>
-          </div>
-          <div className="CreateButtons">
-            <Button
-              className="cancelBtn"
-              variant="primary"
-              onClick={handleClose}
-            >
-              <div className="cancelBtnContent" >Cancel</div>
-            </Button>
-            <Button
-              variant="primary"
-              className="createBtn"
-              type="submit"
-              disabled={isSubmitting}
-            >
-              <div className="createBtnContent">Create</div>
-            </Button>
-          </div>
-        </Form>
-        <div className="rocketPicture">
-        <img src={rocket} alt="rocketPicture" className="rocket"/>
-        <h1 className='rocketSlogan'>Ready? Launch!</h1>
         </div>
-      </div>
       </Modal>
     </>
   );
--- front/src/components/MenuProjectListPopUp.jsx
+++ front/src/components/MenuProjectListPopUp.jsx
@@ -1,4 +1,4 @@
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import Offcanvas from "react-bootstrap/Offcanvas";
 import { useContext } from "react";
 import { StateContext } from "../utils/StateContext";
 import xIcon from "../assets/xIcon.svg";
@@ -6,56 +6,73 @@ import folderPlusIcon from "../assets/folderPlusIcon.svg";
 import plusSquareFillIcon from "../assets/plusSquareFillIcon.svg";
 import searchIcon from "../assets/searchIcon.svg";
 import houseIcon from "../assets/houseIcon.svg";
-import "../styles/projectWithoutList.css";
-import "../styles/projectListPopUp.css";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
-import MenuProjectList from "./MenuProjectList"
+import MenuProjectList from "./MenuProjectList";
+import "../styles/projectWithoutList.css";
+import "../styles/projectListPopUp.css";
 
 function MenuProjectListPopUp() {
-    const {showMenu, setShowMenu, handleShow} = useContext(StateContext)
+  const { showMenu, setShowMenu, handleShow } = useContext(StateContext);
 
-    const handleClose = () => setShowMenu(false);
+  const handleClose = () => setShowMenu(false);
 
   return (
     <>
-      <Offcanvas show={showMenu} onHide={handleClose} backdrop={false}>
-        <Offcanvas.Body>
-        <div className="ProjectPopUp">
-        <div className="home-icon">
-          <div className="HouseIconText">
-          <img src={houseIcon} alt="HouseIcon" className="HouseIcon"/>
-            <span className="buttonthing">Home</span>
-          </div>
-          <button className="closeButton" onClick={handleClose}>
-          <img src={xIcon} alt="X" />
-          </button>
-        </div>
-        <div className="SearchThing">
-          <InputGroup size="sm" className="mb-3">
-            <InputGroup.Text id="inputGroup-sizing-sm">
-            <img src={searchIcon} alt="searchIcon" />
-            </InputGroup.Text>
-            <Form.Control
-              aria-label="Small"
-              placeholder="Search"
-              aria-describedby="inputGroup-sizing-sm"
-            />
-          </InputGroup>
-        </div>
-        <div className="FolderTextThing">
-          <div className="menu-my--projects">
-            <img src={folderPlusIcon} alt="folderIcon" className="FolderIcon2"/>
-            <p className="my-projects--text">My Projects</p>
-          </div>
+      <Offcanvas
+        show={showMenu}
+        onHide={handleClose}
+        backdrop={false}
+        bsPrefix="ProjectPopUp"
+      >
+        <Offcanvas.Body bsPrefix="popup-content">
+          <div className="PopupHeader">
+            <div className="home-icon">
+              <div className="HouseIconText">
+                <img src={houseIcon} alt="HouseIcon" className="HouseIcon" />
+                <span className="buttonthing">Home</span>
+              </div>
+              <button className="closeButton" onClick={handleClose}>
+                <img src={xIcon} alt="X" />
+              </button>
+            </div>
+            <div className="SearchThing">
+              <InputGroup size="sm" className="Epic">
+                <InputGroup.Text
+                  id="inputGroup-sizing-sm"
+                  bsPrefix="SearchIcon"
+                >
+                  <img
+                    src={searchIcon}
+                    alt="searchIcon"
+                    className="Searchicon"
+                  />
+                </InputGroup.Text>
+                <Form.Control
+                  aria-label="Small"
+                  placeholder="Search"
+                  aria-describedby="inputGroup-sizing-sm"
+                  bsPrefix="SearchPopUp"
+                />
+              </InputGroup>
+            </div>
+            <div className="FolderTextThing">
+              <div className="menu-my--projects">
+                <img
+                  src={folderPlusIcon}
+                  alt="folderIcon"
+                  className="FolderIcon2"
+                />
+                <p className="my-projects--text">My Projects</p>
+              </div>
               <div className="PlusIcon">
                 <button className="AddProjectButton" onClick={handleShow}>
-                <img src={plusSquareFillIcon} alt="squareIcon" />
+                  <img src={plusSquareFillIcon} alt="squareIcon" />
                 </button>
               </div>
-        </div>
-        <MenuProjectList/>
-      </div>
+            </div>
+            <MenuProjectList />
+          </div>
         </Offcanvas.Body>
       </Offcanvas>
     </>
