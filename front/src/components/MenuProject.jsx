27a75477f452c8405c642709567ad0a7bf75d2ec
Migle8 <miglekpc3@gmail.com>
2024-04-26 15:06:39 +0300

    Updated create project css

--- front/src/components/MenuProject.jsx
+++ front/src/components/MenuProject.jsx
@@ -11,6 +11,7 @@ import { updateData } from "../services/update";
 import { deleteData } from "../services/delete";
 import { StateContext } from "../utils/StateContext";
 import { useContext } from "react";
+import styles from "../styles/MenuProject.module.css";
 
 function MenuProject({ project }) {
   const { setUpdate } = useContext(StateContext)
@@ -27,8 +28,8 @@ function MenuProject({ project }) {
   const handleDelClose = () => setDelShow(false);
   const handleDelShow = () => setDelShow(true);
 
-  const handleSmAllClose = () => {handleSmShow(), handleClose()};
-  const handleDelAllClose = () => {handleDelShow(), handleClose()};
+  const handleSmAllClose = () => { handleSmShow(), handleClose() };
+  const handleDelAllClose = () => { handleDelShow(), handleClose() };
 
 
   const { projectName, icon } = project;
@@ -80,127 +81,133 @@ function MenuProject({ project }) {
     }
   }
 
+  const {menuProjectList, menuProjectIcon, menuProjectName, editIcon, menuProjectModalBackDrop, menuEdit, menuPencilsquare, menuTrash} = styles;
+
   return (
     <>
-      <div className="MenuProjectList">
-        <div className="MenuProjectList">
-        <img src={icon} alt="icon" className="MenuProjectIcon" />
-        <p className="MenuProjectName">{projectName}</p>
+      <div className={menuProjectList}>
+        <div className={menuProjectList}>
+          <img src={icon} alt="icon" className={menuProjectIcon} />
+          <p className={menuProjectName}>{projectName}</p>
         </div>
         <div>
-        <DashSquare className="editIcon"
-          onClick={handleShow}
-        />
+          <DashSquare className={editIcon}
+            onClick={handleShow}
+          />
 
         </div>
-        <Modal className="myModal" show={show} onHide={handleClose} backdropClassName="menuProjectModalBackDrop">
-        <div className="menu-edit" onClick={handleSmAllClose} >
-            <PencilSquare className="menu-pencilsquare" /> Edit project
+        <Modal
+        className="myModal"
+        // className={myModal}
+        // dialogClassName={`${myModal} modal-content`}
+        show={show} onHide={handleClose} backdropClassName={menuProjectModalBackDrop}>
+          <div className={menuEdit} onClick={handleSmAllClose} >
+            <PencilSquare className={menuPencilsquare} /> Edit project
           </div>
 
           <div className="menu-edit" onClick={handleDelAllClose}>
-              <Trash className="menu-trash" /> Delete project
-            </div>
+            <Trash className={menuTrash} /> Delete project
+          </div>
         </Modal>
 
-          <Modal className="mySecondModal" show={smShow} >
-            <div className="create-project">
-              <h1 className="H12">Edit your project</h1>
-              <Form
-                onSubmit={handleSubmit(formSubmitHandler)}
-                className="create-project--form"
-              >
-                <div>
-                  <Form.Group
-                    className="NewProjectName"
-                    controlId="exampleForm.ControlInput1"
-                  >
-                    <Form.Label>Project Name</Form.Label>
-                    <Form.Control
-                      type="textarea"
-                      placeholder="New project"
-                      name="projectName"
-                      // autoComplete="projectName"
-                      {...register("projectName", {
-                        required: "Project name is required",
-                      })}
-                      isInvalid={errors.projectName}
-                    />
-                    <Form.Control.Feedback type="invalid">
-                      {errors.projectName && errors.projectName.message}
-                    </Form.Control.Feedback>
-                  </Form.Group>
-                </div>
-                <div>
-                  <p className="create-choose--icon">
-                    Choose your project icon
-                  </p>
-                  <IconList />
-                </div>
-                <div>
-                  <Form.Group
-                    className="NewProjectDesc"
-                    controlId="exampleForm.ControlTextarea1"
-                  >
-                    <Form.Label>Project description</Form.Label>
-                    <Form.Control
-                      type="textarea"
-                      as="textarea"
-                      rows={3}
-                      placeholder="Project description"
-                      name="description"
-                      // autoComplete="description"
-                      {...register("description", {
-                        required: "Project description is required",
-                      })}
-                      isInvalid={errors.description}
-                    />
-                    <Form.Control.Feedback type="invalid">
-                      {errors.description && errors.description.message}
-                    </Form.Control.Feedback>
-                  </Form.Group>
-                </div>
-                <div className="CreateButtons">
-                  <Button className="cancelBtn" variant="primary">
-                    <div onClick={handleSmClose} className="cancelBtnContent">
-                      Cancel
-                    </div>
-                  </Button>
-                  <Button
-                    variant="primary"
-                    className="createBtn"
-                    type="submit"
-                    disabled={isSubmitting}
-                  >
-                    <div className="createBtnContent">
-                      Edit
-                    </div>
-                  </Button>
-                </div>
-              </Form>
-            </div>
-          </Modal>
-
-
-            <Modal
-              className="myDeleteModal"
-              show={delShow}
+        <Modal className="mySecondModal" show={smShow} >
+          <div className="create-project">
+            <h1 className="H12">Edit your project</h1>
+            <Form
+              onSubmit={handleSubmit(formSubmitHandler)}
+              className="create-project--form"
             >
-              <Modal.Body>
-                Are You sure You want to delete this project?
-              </Modal.Body>
-              <Modal.Footer>
-                <Button className="cancelBtn" onClick={handleDelClose}>
-                  <div className="cancelBtnContent">Cancel</div>
+              <div>
+                <Form.Group
+                  className="NewProjectName"
+                  controlId="exampleForm.ControlInput1"
+                >
+                  <Form.Label>Project Name</Form.Label>
+                  <Form.Control
+                    type="textarea"
+                    placeholder="New project"
+                    name="projectName"
+                    // autoComplete="projectName"
+                    {...register("projectName", {
+                      required: "Project name is required",
+                    })}
+                    isInvalid={errors.projectName}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.projectName && errors.projectName.message}
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </div>
+              <div>
+                <p className="create-choose--icon">
+                  Choose your project icon
+                </p>
+                <IconList />
+              </div>
+              <div>
+                <Form.Group
+                  className="NewProjectDesc"
+                  controlId="exampleForm.ControlTextarea1"
+                >
+                  <Form.Label>Project description</Form.Label>
+                  <Form.Control
+                    type="textarea"
+                    as="textarea"
+                    rows={3}
+                    placeholder="Project description"
+                    name="description"
+                    // autoComplete="description"
+                    {...register("description", {
+                      required: "Project description is required",
+                    })}
+                    isInvalid={errors.description}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.description && errors.description.message}
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </div>
+              <div className="CreateButtons">
+                <Button className="cancelBtn" variant="primary">
+                  <div onClick={handleSmClose} className="cancelBtnContent">
+                    Cancel
+                  </div>
                 </Button>
-                <Button className="createBtn" onClick={() => handleDelete(project._id)}>
-                  Delete
+                <Button
+                  variant="primary"
+                  className="createBtn"
+                  type="submit"
+                  disabled={isSubmitting}
+                >
+                  <div className="createBtnContent">
+                    Edit
+                  </div>
                 </Button>
-              </Modal.Footer>
-            </Modal>
-
-
+              </div>
+            </Form>
           </div>
+        </Modal>
+
+
+        <Modal
+          className="myDeleteModal"
+          show={delShow}
+        >
+          <Modal.Body>
+            Are You sure You want to delete this project?
+          </Modal.Body>
+          <Modal.Footer>
+            <Button className="cancelBtn" onClick={handleDelClose}>
+              <div className="cancelBtnContent">Cancel</div>
+            </Button>
+            <Button className="createBtn" onClick={() => handleDelete(project._id)}>
+              Delete
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
+
+      </div>
     </>
   );
 }
