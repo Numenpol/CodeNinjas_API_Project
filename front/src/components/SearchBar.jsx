1eab89fbcef67af4165c43da7b0c7641ea3953cb
Migle8 <miglekpc3@gmail.com>
2024-05-16 12:39:46 +0300

    Fixed sort css

--- front/src/components/SearchBar.jsx
+++ front/src/components/SearchBar.jsx
@@ -1,15 +1,45 @@
 import "../styles/SearchBar.css";
 import { Search } from "react-bootstrap-icons";
-import Modal from 'react-bootstrap/Modal';
+// import Modal from 'react-bootstrap/Modal';
 import { Sliders, CheckSquareFill, Square } from 'react-bootstrap-icons';
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { createPopper } from '@popperjs/core';
 
 function SearchBar() {
   const [smShow, setSmShow] = useState(false);
   const [checked, setChecked] = useState({ projectName: false, status: false });
+  const buttonRef = useRef(null);
 
   const handleCheck = (name) => {
     setChecked(prevState => ({ ...prevState, [name]: !prevState[name] }));
+  }
+
+  useEffect(() => {
+    let popperInstance;
+
+    if (smShow) {
+      popperInstance = createPopper(buttonRef.current, document.querySelector('.sort-popup'), {
+        placement: 'bottom',
+      });
+    } else {
+      if (popperInstance) {
+        popperInstance.destroy();
+      }
+    }
+
+    return () => {
+      if (popperInstance) {
+        popperInstance.destroy();
+      }
+    };
+  }, [smShow]);
+
+  const handleClick = () => {
+    if (smShow) {
+      setSmShow(false);
+    } else {
+      setSmShow(true);
+    }
   }
   
   return (
@@ -26,13 +56,29 @@ function SearchBar() {
               aria-describedby="button-addon"
               className="form-control rounded-pill border-0 color"
             />
-
             <Sliders
-              onClick={() => setSmShow(true)}
+              // onClick={() => setSmShow(true)}
               // onClick={handleShow}
+              ref={buttonRef}
+              onClick={() => handleClick()}
               className="sliders me-3 d-flex align-self-center" />
+                {smShow && (
+              <div className="sort-popup" >
+                <div className="sort-title">
+                  <p className="modal-title">Choose columns to search</p>
+                </div>
+                <div onClick={() => handleCheck('projectName')}>
+                  {checked.projectName ? <CheckSquareFill className="me-3 check-icon" /> : <Square className="me-3 check-icon-empty" />}
+                  Project name
+                </div>
+                <div onClick={() => handleCheck('status')}>
+                  {checked.status ? <CheckSquareFill className="me-3 check-icon" /> : <Square className="me-3 check-icon-empty" />}
+                  Status
+                </div>
+              </div>
+            )}
           </div>
-          <Modal
+          {/* <Modal
             size="sm"
             show={smShow}
             onHide={() => setSmShow(false)}
@@ -54,7 +100,7 @@ function SearchBar() {
                 Status
               </div>
             </Modal.Body>
-          </Modal>
+          </Modal> */}
         </div>
       </form>
     </>
